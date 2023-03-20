0e8e81f03b0f491c5e59ead14c4b0dd3edb0d645
Lautaro Decima <decimalautaro@gmail.com>
2023-03-20 19:41:05 -0300

    feat: configurando app.jsx

---
+++ front-end/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Footer } from './componets/Footer'
+import { SignIn } from './componets/SignIn'
+
+export const App = ()=> {
+  
+
+  return (
+    <>
+    <SignIn/>
+    <Footer/>
+    </>
+  )
+}
+
+
